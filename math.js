eb8bf9ce949a4b347d6c320fede09cfbd123cd7e
masum9100 <masum.seopro@gmail.com>
2023-08-20 20:17:18 +0600

    item name added

--- math.js
+++ math.js
@@ -1,1 +1,11 @@
-console.log(hello)
+let serialNum = 1;
+
+function handleClickBtn(masum){
+    const selectItemList = document.getElementById("selected-items");
+    const itemName = masum.parentNode.childNodes[1].innerText;
+    const p = document.createElement("p");
+    p.innerText = `${serialNum}-  ${itemName}`;
+    serialNum++;
+    
+    selectItemList.appendChild(p)
+}
